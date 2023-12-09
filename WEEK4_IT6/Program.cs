78c5ac5d8fbb103d7ebe6a825f6045d4cb5c9fd5
ArtZ23 <zzartur77@gmail.com>
2023-12-09 13:38:31 +0400

    Week4 VS first push

---
+++ WEEK4_IT6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WEEK4_IT6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.BackgroundColor = ConsoleColor.Green;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("This is my new project");
+			Console.WriteLine("This is my new project");
+		}
+	}
+}
